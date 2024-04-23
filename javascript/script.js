eca1a1c9924416548a1589da5ce7716fab48e53a
Cyril Muhwati <40686410@live.napier.ac.uk>
2024-04-23 21:35:16 +0200

    tsoro files
    modified the files

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,14 @@
 
-
+// for the button to open the start the game page when clicked
 function start(){
     window.open("start_the_game.html","_self");
 }
-
+// for the drop down menu to appear and dissapear on clicking the tsoro in the header
 let menu=document.getElementById("subMenu");
  function dropMenu(){
     subMenu.classList.toggle("open-menu")
 }
-
-document.getElementById("myh2").style.wordSpacing="17px";
+// for the another_navmenu to beocme fixed and change color as user scroll
 window.addEventListener("scroll",function(){
     var nav = document.querySelector(".another_navmenu");
     nav.classList.toggle("sticky", window.scrollY > 80);
